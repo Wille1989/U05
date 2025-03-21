7580efa9266671137f33ad5740264cafe7ee5856
Wille1989 <W.Lundqvist@live.se>
2025-03-21 14:08:34 +0100

    making some adjustments for better readability

--- src/types/disc.ts
+++ src/types/disc.ts
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 import { IManufacturerBody } from "./manufacturer";
 
 export interface IDisc extends mongoose.Document {
+    manufacturer: mongoose.Types.ObjectId | IManufacturerBody;
     title: string;
     type: "Distance Driver" | "Driver" | "Mid-Range" | "Putter";
-    manufacturer: mongoose.Types.ObjectId | IManufacturerBody;
     speed: number;
     glide: number;
     turn: number;
